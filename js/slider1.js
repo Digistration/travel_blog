92717f9385655b2b4574d640b42257b61988d555
James Devine <digistration@gmail.com>
2019-03-12 10:29:46 +0800

    Initial commit

---
+++ js/slider1.js
@@ -0,0 +1,27 @@
+$('#s1_btn_next').click(function(){
+  var currentSlide = $('.active');
+  var nextSlide = currentSlide.next();
+  var firstSlide = currentSlide.siblings('.first');
+
+  if(nextSlide.length){
+    currentSlide.removeClass('active').css("z-index", -10);
+    nextSlide.addClass('active').css("z-index", 10);
+  } else{
+    currentSlide.removeClass('active').css("z-index", -10);
+    firstSlide.addClass('active').css("z-index", 10);
+  }
+});
+
+$('#s1_btn_prev').click(function(){
+  var currentSlide = $('.active');
+  var prevSlide = currentSlide.prev();
+  var lastSlide = currentSlide.siblings('.last');
+
+  if(prevSlide.length){
+    currentSlide.removeClass('active').css("z-index", -10);
+    prevSlide.addClass('active').css("z-index", 10);
+  } else{
+    currentSlide.removeClass('active').css("z-index", -10);
+    lastSlide.addClass('active').css("z-index", 10);
+  }
+});
